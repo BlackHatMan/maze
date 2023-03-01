0db5d249a456f195017e8cde8aa86a8649e226c6
Victor <ecartman41@gmail.com>
2023-03-01 13:33:38 +0300

    add task1

---
+++ task1.js
@@ -0,0 +1,36 @@
+function bracketsParser(expr) {
+  const stack = [];
+
+  for (let i = 0; i < expr.length; i++) {
+    let x = expr[i];
+
+    if (x === '(' || x === '[' || x === '{') {
+      stack.push(x);
+      continue;
+    }
+
+    if (stack.length === 0) return false;
+
+    let check;
+    switch (x) {
+      case ')':
+        check = stack.pop();
+        if (check === '{' || check === '[') return false;
+        break;
+
+      case '}':
+        check = stack.pop();
+        if (check === '(' || check === '[') return false;
+        break;
+
+      case ']':
+        check = stack.pop();
+        if (check === '(' || check === '{') return false;
+        break;
+      default:
+        break;
+    }
+  }
+
+  return stack.length === 0;
+}
